d2948eb4432e10ce710d65dd8b0c14d8e19ed44b
Shimpei Matsuda <shinpeimatsuda.june07@gmail.com>
2018-01-23 14:49:18 +0800

    1/23

---
+++ wp-content/themes/hitmag/templates/full-width.php
@@ -0,0 +1,45 @@
+<?php 
+
+
+/* Template Name: Cool template */
+
+get_header();
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+ <div id="content" class="site-content">	
+	<div class="hm-container">
+	 	<div id="primary" class="content-area">
+	 		<main id="main" class="site-main" role="main">
+
+				
+	<article id="post-7" class="hitmag-page post-7 page type-page status-publish hentry">
+		<header class="entry-header">
+			<h1 class="entry-title">Portfolio</h1>	</header><!-- .entry-header -->
+
+		
+		<div class="entry-content">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod<br>
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,<br>
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo<br>
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse<br>
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non<br>
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div><!-- .entry-content -->
+
+				<footer class="entry-footer">
+				<span class="edit-link"><a class="post-edit-link" href="http://localhost/wpshimpei/wp-admin/post.php?post=7&amp;action=edit">Edit <span class="screen-reader-text">"Portfolio"</span></a></span>		</footer><!-- .entry-footer -->
+		</article><!-- #post-## -->
+
+			</main>
+		</div>
+	</div>		 	
+</div>	 
+ </body>
+ </html>
+ <?php
+ get_footer();
